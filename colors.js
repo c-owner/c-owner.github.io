e5f777d800737669421606ce77e494329705ce95
Eight-Corner <68332735+Eight-Corner@users.noreply.github.com>
2020-07-23 21:31:24 +0900

    Add files via upload

--- colors.js
+++ colors.js
@@ -1,36 +1,39 @@
 var Links = {
   setColor:function(color){
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
-      alist[i].style.color = color;
-      i = i + 1;
-    }
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length){
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    // }
+    $('a').css('color', color);
   }
 }
 var Body = {
   setColor:function (color){
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color);
   },
-setBackgroundColor:function(color){
-  document.querySelector('body').style.backgroundColor = color;
-}
+  setBackgroundColor:function(color){
+  // document.querySelector('body').style.backgroundColor = color;
+  $('body').css('backgroundColor', color);
+  }
 }
 function nightDayTogle(self){
-var target = document.querySelector('body');
-if(target.dataset.mode === 'day'){
-  Body.setBackgroundColor('black');
-  Body.setColor('white');
-  target.dataset.mode = 'night';
-  self.value = '라이트모드';
+  var target = document.querySelector('body');
+  if(target.dataset.mode === 'day'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    target.dataset.mode = 'night';
+    self.value = '라이트모드';
 
-  Links.setColor('powderblue');
-} else {
-  Body.setBackgroundColor('white');
-  Body.setColor('black');
-  target.dataset.mode = 'day';
-  self.value = '다크모드';
+    Links.setColor('powderblue');
+}    else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    target.dataset.mode = 'day';
+    self.value = '다크모드';
 
-  Links.setColor('black');
-}
+    Links.setColor('black');
+  }
 }
